7842cac1031da90d4c3ce6320c93858e8da88f01
As'ad Saleh Umar <azzzad.s@gmail.com>
2023-03-18 09:35:15 +0700

    create timer and callback

---
+++ chapter_7_2_async/quiz/timer.js
@@ -0,0 +1,18 @@
+// Hi guys! Welcome to another JS Quiz!
+// Di sini kita akan belajar tentang penggunaan setTimeout dan setInterval.
+
+// 1. Buat code yang melakukan console.log("Hello World") setiap 3 detik:
+// hint gunakan fungsi setInterval()
+
+// 2. Buat code yang melakukan console.log(detik) setiap 1 detik:
+// hint gunakan fungsi setInterval()
+
+// 3. Buat code yang melakukan console.log("Aku terpanggil setelah 3 detik") setelah 3 detik berlalu.
+// hint gunakan fungsi setTimeout()
+
+// 4. Buat code yang melakukan console.log("Aku terpanggil setelah 10 detik") setelah 10 detik beralalu.
+// hint gunakan fungsi setTimeout()
+
+// 5. (Final Boss) Buat code yang melakukan console.log(detik) setiap 1 detik selama 10 detik, lalu
+// tidak melakukan apa-apa selama 5 detik, lalu melakukan console.log(detik) setiap 2 detik selama
+// 5 detik, lalu selesai (tidak melakukan apa-apa).
